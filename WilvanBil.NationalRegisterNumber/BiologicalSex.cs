2d907085ee6b5ff478fa1587489d841bfdc1b2d1
Willy Alde <willy.alde@gmail.com>
2025-01-22 21:58:51 +0100

    chore: Added summary for `BiologicalSex` enum.

--- WilvanBil.NationalRegisterNumber/BiologicalSex.cs
+++ WilvanBil.NationalRegisterNumber/BiologicalSex.cs
@@ -1,5 +1,8 @@
 ﻿namespace WilvanBil.NationalRegisterNumber;
 
+/// <summary>
+/// The biological sex of the person as given by the government.
+/// </summary>
 public enum BiologicalSex
 {
     Female,
