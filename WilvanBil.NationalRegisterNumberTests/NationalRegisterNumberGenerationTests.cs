6d4a16e562b4551da35d4a6c857c0a78a6b60aa2
Willy Alde <willy.alde@gmail.com>
2025-01-29 21:58:20 +0100

    chore: Segregated unit tests on functionality and commented `csproj` for clarification.

--- WilvanBil.NationalRegisterNumberTests/NationalRegisterNumberGenerationTests.cs
+++ WilvanBil.NationalRegisterNumberTests/NationalRegisterNumberGenerationTests.cs
@@ -1,42 +1,10 @@
 using FluentAssertions;
-using FsCheck.Xunit;
 using Xunit;
 
 namespace WilvanBil.NationalRegisterNumber.UnitTests;
 
-public class NationalRegisterNumberTests
+public class NationalRegisterNumberGenerationTests
 {
-    [Theory]
-    [InlineData("90022742191")]
-    public void ValidateShouldReturnTrue(string nationalRegisterNumber)
-    {
-        // Act
-        var result = NationalRegisterNumberGenerator.IsValid(nationalRegisterNumber);
-
-        // Assert
-        result.Should().BeTrue();
-    }
-
-    [Theory]
-    [InlineData("12345678910")]
-    [InlineData("12345621748")]
-    [InlineData("12345621777")]
-    [InlineData("Test")]
-    [InlineData("00000000000")]
-    [InlineData("99999999999")]
-    [InlineData("!@#!@%^@^@$^&@$^@sdfasdf")]
-    [InlineData("$^@#^@##$44")]
-    [InlineData("15435#$%4354dfsg")]
-    [InlineData("90022742192")]
-    public void ValidateShouldReturnFalse(string nationalRegisterNumber)
-    {
-        // Act
-        var result = NationalRegisterNumberGenerator.IsValid(nationalRegisterNumber);
-
-        // Assert
-        result.Should().BeFalse();
-    }
-
     [Fact]
     public void GenerateWithBirthdateAndFollowNumberShouldWork()
     {
@@ -50,21 +18,6 @@ public class NationalRegisterNumberTests
         // Assert
         var assertion = NationalRegisterNumberGenerator.IsValid(result);
         assertion.Should().BeTrue();
-    }
-
-    /// <summary>
-    /// Ensures the NationalRegisterNumberGenerator.Generate method produces valid results for any DateTime input.
-    /// FsCheck generates a wide range of random and edge-case DateTime values for testing.
-    /// </summary>
-    /// <param name="datetime">The randomly generated DateTime input.</param>
-    [Property(Verbose = true)]
-    public void GenerateShouldWorkForAnyDateTime(DateTime datetime)
-    {
-        // Act
-        var nationalRegisterNumber = NationalRegisterNumberGenerator.Generate(DateOnly.FromDateTime(datetime));
-
-        // Assert
-        Assert.True(NationalRegisterNumberGenerator.IsValid(nationalRegisterNumber));
     }
 
     [Fact]
---
+++ WilvanBil.NationalRegisterNumberTests/NationalRegisterNumberValidationTests.cs
@@ -0,0 +1,54 @@
+﻿using FluentAssertions;
+using FsCheck.Xunit;
+using Xunit;
+
+namespace WilvanBil.NationalRegisterNumber.UnitTests;
+
+public class NationalRegisterNumberValidationTests
+{
+    [Theory]
+    [InlineData("90022742191")]
+    public void ValidateShouldReturnTrue(string nationalRegisterNumber)
+    {
+        // Act
+        var result = NationalRegisterNumberGenerator.IsValid(nationalRegisterNumber);
+
+        // Assert
+        result.Should().BeTrue();
+    }
+
+    [Theory]
+    [InlineData("12345678910")]
+    [InlineData("12345621748")]
+    [InlineData("12345621777")]
+    [InlineData("Test")]
+    [InlineData("00000000000")]
+    [InlineData("99999999999")]
+    [InlineData("!@#!@%^@^@$^&@$^@sdfasdf")]
+    [InlineData("$^@#^@##$44")]
+    [InlineData("15435#$%4354dfsg")]
+    [InlineData("90022742192")]
+    public void ValidateShouldReturnFalse(string nationalRegisterNumber)
+    {
+        // Act
+        var result = NationalRegisterNumberGenerator.IsValid(nationalRegisterNumber);
+
+        // Assert
+        result.Should().BeFalse();
+    }
+
+    /// <summary>
+    /// Ensures the NationalRegisterNumberGenerator.Generate method produces valid results for any DateTime input.
+    /// FsCheck generates a wide range of random and edge-case DateTime values for testing.
+    /// </summary>
+    /// <param name="birthDate">The randomly generated DateTime input.</param>
+    [Property(Verbose = true)]
+    public void GenerateShouldWorkForAnyDateTime(DateTime birthDate)
+    {
+        // Act
+        var nationalRegisterNumber = NationalRegisterNumberGenerator.Generate(DateOnly.FromDateTime(birthDate));
+
+        // Assert
+        Assert.True(NationalRegisterNumberGenerator.IsValid(nationalRegisterNumber));
+    }
+}
